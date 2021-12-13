a580ea84a01ca2cc664b862ac4be52b9b6277072
Nguyễn Trần Nhật Đức <ducnhat.nguyentran.23.05@gmail.com>
2021-12-13 09:08:07 +0700

    update

--- todo-list/app/src/main/java/com/example/todolist/MyDayActivity.java
+++ todo-list/app/src/main/java/com/example/todolist/MyDayActivity.java
@@ -1,14 +1,27 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MyDayActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TaskMydayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_day);
+        recyclerView = findViewById(R.id.rc_myday);
+
+        adapter = new TaskMydayAdapter();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        
     }
 }
---
+++ todo-list/app/src/main/java/com/example/todolist/TaskMydayAdapter.java
@@ -0,0 +1,52 @@
+package com.example.todolist;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskMydayAdapter extends RecyclerView.Adapter<TaskMydayAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View viewListItem = inflater.inflate(R.layout.item_task, parent, false);
+        return new ViewHolder(viewListItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String nameTask = "Test "+ position;
+        holder.tvName.setText(nameTask);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private View listItemView;
+        public RadioButton radioDone;
+        public TextView tvName;
+        public Button btStar;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            listItemView = itemView;
+            radioDone = listItemView.findViewById(R.id.radio_done);
+            tvName = listItemView.findViewById(R.id.tv_task);
+            btStar = listItemView.findViewById(R.id.bt_star);
+
+
+
+        }
+    }
+}
